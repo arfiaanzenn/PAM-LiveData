81dcae0df779b0a0614849232ee361e58bd89c1f
arfiaanzenn <mhfarhianalmz@gmail.com>
2024-10-26 17:32:03 +0700

    Initial commit

---
+++ app/src/main/java/lat/pam/prak_pam/NameViewModel.kt
@@ -0,0 +1,17 @@
+package lat.pam.prak_pam
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+
+class NameViewModel : ViewModel() {
+
+
+    // Create a LiveData with a String
+    val currentName: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+
+    // Rest of the ViewModel...
+}
